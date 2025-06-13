dca20c9bfdd51566988eced2f5ddafb3650882ec
rezniki <igorreznik2001@gmail.com>
2025-06-13 21:21:31 +0300

    PropertyCard, Home, App.css / add style cards

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PropertyCard from "../../components/PropertyCard/PropertyCard";
 import { properties } from "../../mock/properties";
+import "./style.css";
 
 const Home: React.FC = () => {
     return (
         <>
             <div className="home-page">
-                <h1>Luxury Properties</h1>
+                <h1 className="home-title">Luxury Properties</h1>
                 <div className="property-grid">
                     {properties.map((property) => (
                         <PropertyCard key={property.id} property={property} />
