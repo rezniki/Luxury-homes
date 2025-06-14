5abe8469452bbd4689c91bca1e4780d39491ecd0
rezniki <igorreznik2001@gmail.com>
2025-06-14 17:37:07 +0300

    App.tsx, Header / add header and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+        <Header/>
+        <Home/>
+      </BrowserRouter>
+      
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,21 @@
-export {}
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+const Header: React.FC = () => {
+    return (
+        <>
+            <header className="luxury-header">
+                <div className="luxury-header-container">
+                    <h1 className="luxury-capsule-logo">Luxury Homes Capsule</h1>
+                    <nav className="luxury-capsule-nav">
+                        <Link to="/">Home</Link>
+                        <Link to="/favorites">Favorites</Link>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+};
+
+export default Header
